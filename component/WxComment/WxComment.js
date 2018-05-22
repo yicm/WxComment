b4daa2c5a99f9116dfe923abefe24e231791f312
Ethanm <yicm1102@gmail.com>
2018-05-22 14:47:51 +0800

    Add Liked Feature

--- component/WxComment/WxComment.js
+++ component/WxComment/WxComment.js
@@ -1,8 +1,12 @@
+/*
+ * author: yicm1102@gmail.com
+ */
+
 const AV = require('../../libs/leancloud/av-weapp-min.js');
 // LeanCloud 应用的 ID 和 Key
 AV.init({
-  appId: 'HRYpYqLTygyfRlsN8I3KEhkw-gzGzoHsz',
-  appKey: '5two7AvBo3gjnH3hpYSyBW35',
+  appId: 'your leancloud appid',
+  appKey: 'your leancloud appkey',
 });
 
 Component({
@@ -23,33 +27,55 @@ Component({
       type: String,
       value: '',
       observer: function (newVal, oldVal) {
+        //console.log('observer...');
         var that = this;
-        var query = new AV.Query('WxComment');
-        query.equalTo('article_id', this.data.articleID);
-        // descending:降序/ascending:升序
-        query.descending('updateAt');
-        // 同时查询包含对象Pointer的详细信息
-        query.include('targetUser');
-        query.find().then(function (results) {
-          //console.log(results);
-          // 处理初次加载的评论
-          for (var i = 0; i < results.length; i++) {
-            var item = {};
-            item['id'] = results[i].id;
-            item['userId'] = results[i].attributes.targetUser.id;
-            item['articleID'] = results[i].attributes.article_id;
-            item['nickName'] = results[i].attributes.targetUser.attributes.nickName;
-            item['avatarUrl'] = results[i].attributes.targetUser.attributes.avatarUrl;
-            item['content'] = results[i].attributes.content;
-            item['time'] = results[i].attributes.time;
-            item['at'] = results[i].attributes.at;
+        AV.User.loginWithWeapp().then(user => {
+          that.data.leancloud_user_id = user.id;
+          that.data.login_user_info = user.toJSON();
 
-            that.data.leancloud_comment_data.push(item);
-          }
-          that.setData({
-            leancloud_comment_data: that.data.leancloud_comment_data,
-            comment_num: results.length
-          });
+          var query = new AV.Query('WxComment');
+          query.equalTo('article_id', that.data.articleID);
+          // descending:降序/ascending:升序
+          query.descending('updateAt');
+          // 同时查询包含对象Pointer的详细信息
+          query.include('targetUser');
+          query.include('targetZan');
+          query.find().then(function (results) {
+            // 处理初次加载的评论
+            for (var i = 0; i < results.length; i++) {
+              var item = {};
+              item['id'] = results[i].id;
+              item['userId'] = results[i].attributes.targetUser.id;
+              item['zanId'] = results[i].attributes.targetZan.id;
+
+              var zanUserList = results[i].attributes.targetZan.attributes.userList;
+
+              item['zanCurrent'] = false;
+              for (var j = 0; j < zanUserList.length; j++) {
+                if (zanUserList[j] == that.data.leancloud_user_id) {
+                  item['zanCurrent'] = true;
+                  break;
+                } else {
+                  item['zanCurrent'] = false;
+                }
+              }
+
+              item['zanNum'] = results[i].attributes.targetZan.attributes.zan;
+              item['articleID'] = results[i].attributes.article_id;
+              item['nickName'] = results[i].attributes.targetUser.attributes.nickName;
+              item['avatarUrl'] = results[i].attributes.targetUser.attributes.avatarUrl;
+              item['content'] = results[i].attributes.content;
+              item['time'] = results[i].attributes.time;
+              item['at'] = results[i].attributes.at;
+
+              that.data.leancloud_comment_data.push(item);
+            }
+            //console.log(that.data.leancloud_comment_data);
+            that.setData({
+              leancloud_comment_data: that.data.leancloud_comment_data,
+              comment_num: results.length
+            });
+          }).catch(console.error);
         }, function (error) {
           wx.showToast({
             title: '评论加载失败！',
@@ -71,8 +97,10 @@ Component({
     comment_num: 0,
     show_aur_button: false,
     user_info: [],
+    leancloud_user_id: '',
     login_user_info: [],
-    leancloud_comment_data: []
+    leancloud_comment_data: [],
+    leancloud_comment_zan_data: []
   },
   methods: {
     // 事件响应函数
@@ -90,7 +118,7 @@ Component({
       }
 
       //console.log(that.data.articleID);
-      that.data.comment_id = that.data.articleID;
+      that.data.comment_id = that.data.articleID; ''
       that.data.comment_data = e.detail.value.comment_text;
 
       // 获取用户信息
@@ -191,7 +219,71 @@ Component({
         }
       })
     },
-    onGotUserInfo: function (e) {
+    _updateZanShow: function (mode, comment_id) {
+      var that = this;
+      for (var i = 0; i < that.data.leancloud_comment_data.length; i++) {
+        if (that.data.leancloud_comment_data[i].id == comment_id) {
+          if (mode == 'cancel') {
+            that.data.leancloud_comment_data[i].zanNum = that.data.leancloud_comment_data[i].zanNum - 1;
+            that.data.leancloud_comment_data[i].zanCurrent = false;
+          }
+          else if (mode == 'submit') {
+            that.data.leancloud_comment_data[i].zanNum = that.data.leancloud_comment_data[i].zanNum + 1;
+            that.data.leancloud_comment_data[i].zanCurrent = true;
+          }
+          break;
+        }
+      }
+      that.setData({
+        leancloud_comment_data: that.data.leancloud_comment_data
+      })
+    },
+    _writeZanInLeanCloud: function (user_id, comment_id, zan_id) {
+      var that = this;
+      // 查询当前用户是否已点赞
+      var query = new AV.Query('Zan');
+      var search = [that.data.leancloud_user_id];
+      query.equalTo('commentObjId', comment_id);
+      query.containsAll('userList', search);
+      query.find().then(function (results) {
+        //console.log(results);      
+        if (results.length == 1) {
+          // 当前用户已给该评论点赞，取消赞
+          var op_str = "update Zan set zan=op('Decrement', {'amount': 1}),userList=op('Remove', {'objects':[\"" + that.data.leancloud_user_id + "\"]}) where objectId='" + zan_id + "'";
+          AV.Query.doCloudQuery(op_str).then(function (data) {
+            // data 中的 results 是本次查询返回的结果，AV.Object 实例列表
+            // 更新显示, -1
+            //console.log('update zan cancel');
+            that._updateZanShow('cancel', comment_id);
+          }, function (error) {
+            // 异常处理
+            console.error(error);
+          });
+        }
+        else {
+          // 当前用户还未给该评论点赞
+          var op_str = "update Zan set zan=op('Increment', {'amount': 1}),userList=op('AddUnique', {'objects':[\"" + that.data.leancloud_user_id + "\"]}) where objectId='" + zan_id + "'";
+          AV.Query.doCloudQuery(op_str).then(function (data) {
+            // data 中的 results 是本次查询返回的结果，AV.Object 实例列表
+            // 更新显示
+            //console.log('update zan submit');
+            that._updateZanShow('submit', comment_id);
+          }, function (error) {
+            // 异常处理
+            console.error(error);
+          });
+        }
+      }).catch(console.error);
+    },
+    zanCommentClick: function (e) {
+      var that = this;
+      // user_id为评论发布者的用户id，非当前用户id
+      var user_id = e.currentTarget.dataset.user_id;
+      var comment_id = e.currentTarget.dataset.comment_id;
+      var zan_id = e.currentTarget.dataset.zan_id;
+      that._writeZanInLeanCloud(user_id, comment_id, zan_id);
+    },
+    onGetUserInfo: function (e) {
       var that = this;
       if (e.detail.userInfo) {
         console.log(e.detail.userInfo);
@@ -251,8 +343,10 @@ Component({
     // 自定义方法
     _writeCommentInLeanCloud: function () {
       var that = this;
+      // new WxComment
       var WxComment = AV.Object.extend('WxComment');
       var wxcomment = new WxComment();
+
       var current_time = that._getTime();
       const user = AV.User.current();
       //console.log(that.data.login_user_info);
@@ -263,7 +357,37 @@ Component({
       wxcomment.set('at', '');
       var targetUser = AV.Object.createWithoutData('_User', user.id);
       wxcomment.set('targetUser', targetUser);
+
       wxcomment.save().then(function (wxcomment) {
+        // new Zan
+        var Zan = AV.Object.extend('Zan');
+        var zan = new Zan();
+        zan.set('zan', 0);
+        zan.set('commentObjId', wxcomment.id);
+        zan.set('userList', []);
+        zan.save().then(function (zan) {
+          var targetZan = AV.Object.createWithoutData('Zan', zan.id);
+          wxcomment.set('targetZan', targetZan);
+          wxcomment.save().then(function (wxcomment) {
+            // 评论和赞处理完毕
+            // do something...
+            console.log("评论和赞处理完毕");
+          }), function (error) {
+            wx.showToast({
+              title: '评论处理失败！',
+              icon: 'none',
+              duration: 2000
+            })
+          }
+        }), function (error) {
+          // 异常处理
+          wx.showToast({
+            title: '赞初始化失败！',
+            icon: 'none',
+            duration: 2000
+          })
+        }
+
         // 成功保存之后，执行其他逻辑.
         wx.showToast({
           title: '评论成功！',
@@ -281,6 +405,7 @@ Component({
         current_comment['time'] = current_time;
         current_comment['content'] = that.data.comment_data;
         current_comment['at'] = '';
+        current_comment['zanNum'] = 0;
         that.data.leancloud_comment_data.push(current_comment);
         that.setData({
           leancloud_comment_data: that.data.leancloud_comment_data,
